ce7c2c8c722f67fd875c2f847217aec97f77e1af
erinc1915 <34071496+erinc1915@users.noreply.github.com>
2018-10-22 11:35:20 -0500

    Add files via upload

---
+++ source/canvas.js
@@ -0,0 +1,10 @@
+/*var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+*/
+(function () {
+  var doc = document.getElementById('doc');
+  doc.contentEditable = true;
+  doc.focus();
+})();
